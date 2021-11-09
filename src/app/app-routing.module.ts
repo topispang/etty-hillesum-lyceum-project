8f6cbbc99ccee4f4c7f9a078435d8dc7c75182b7
topispang <dennis.spangenberg@topicus.nl>
2021-11-09 09:11:37 +0100

    1 binaries uploaded

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { InputComponent } from './input/input.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: 'input', component: InputComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'input', component: InputComponent },
+];
 
 @NgModule({
-  imports: [[RouterModule.forRoot(routes)]],
-  declarations: [RouterModule],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
